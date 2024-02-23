c37fe838a5919465dcb43754c0296de2b83b681f
Jucelio coelho <juceliocoelho2022@gmail.com>
2024-02-23 06:41:11 -0300

    Initial commit

---
+++ src/main/kotlin/com/Dev_Jucelio/womensworldcup2023/WomensWorldCup2023Application.kt
@@ -0,0 +1,11 @@
+package com.Dev_Jucelio.womensworldcup2023
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WomensWorldCup2023Application
+
+fun main(args: Array<String>) {
+	runApplication<WomensWorldCup2023Application>(*args)
+}
---
+++ src/test/kotlin/com/Dev_Jucelio/womensworldcup2023/WomensWorldCup2023ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Dev_Jucelio.womensworldcup2023
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WomensWorldCup2023ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
